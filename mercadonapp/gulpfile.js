448a6b9285e1f121313b49c6962f6d09c65da7b9
Clément Rocolle <clem.roc5@gmail.com>
2023-04-11 23:41:36 +0200

    started the catalog page

--- mercadonapp/gulpfile.js
+++ mercadonapp/gulpfile.js
@@ -1,12 +1,15 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass')(require('sass')),
     rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    gutil = require('gulp-util'),
+    cleanCSS = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     autoprefixer = require('gulp-autoprefixer'),
     webp = require('gulp-webp');
-
 
 var paths = {
     src: './static/',
@@ -15,8 +18,27 @@ var paths = {
     npm: 'node_modules/'
 };
 
+var minifyIfNeeded = function (alwaysMinify) {
+    return alwaysMinify === true || gutil.env.env === 'prod' || gutil.env.env === 'preprod' ? cleanCSS({processImport: false}) : gutil.noop();
+}
+
+var uglifyIfNeeded = function(alwaysUglify) {
+    return alwaysUglify === true || gutil.env.env === 'prod' || gutil.env.env === 'preprod' ? uglify() : gutil.noop();
+}
+
+var errorHandler = function (err) {
+    if (typeof err.file !== 'undefined') {
+        console.log(err.file, err.line, err.messageOriginal);
+    } else {
+        console.log(err);
+    }
+    this.emit('end');
+}
+
 gulp.task('js', function () {
     return gulp.src(paths.src + 'js/**/*.*')
+        .pipe(plumber({ errorHandler: errorHandler }))
+        .pipe(uglifyIfNeeded())
         .pipe(concat('app.js'))
         .pipe(gulp.dest(paths.dest + 'js'));
 });
@@ -28,8 +50,10 @@ gulp.task('fonts', function() {
 
 gulp.task('css', function () {
     return gulp.src(paths.src + 'scss/app.scss')
+        .pipe(plumber({ errorHandler: errorHandler }))
         .pipe(sass())
         .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+        .pipe(minifyIfNeeded(true))
         .pipe(concat('app.css'))
         .pipe(gulp.dest(paths.dest + 'css'));
 });
