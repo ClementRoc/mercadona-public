d2b44ceb8ed10538fce8b0e0ff3dd341c29d4829
Clément Rocolle <clem.roc5@gmail.com>
2023-04-25 17:08:37 +0200

    removed config.py from gitignore

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+APP_ENV = os.getenv('APP_ENV', 'development')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'Panribou1')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+CONTENTFUL_SPACE_ID = 'ivu7p66elrjd'
+CONTENTFUL_ACCESS_TOKEN = 'DQPKTv3cLkLLYkiZdubD4vrLAQgy0q1tVpepW3mbSKM'
+
+del os
