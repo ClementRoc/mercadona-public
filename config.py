4636c791c4a4274d9f54368549cbe3ca4f1ff2ed
Clément Rocolle <clem.roc5@gmail.com>
2023-05-20 15:37:00 +0200

    troubleshooting heroku

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 APP_ENV = os.getenv('APP_ENV', 'development')
 DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'Panribou1')
-DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'mercadona-456.herokuapp.com')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
 
 SQLALCHEMY_DATABASE_URI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
