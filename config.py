1877fe8213b2c5837da668318bfaf9fc59dede74
Clément <clem.roc5@gmail.com>
2023-05-21 02:00:53 +0200

    Troubleshooting Heroku

--- config.py
+++ config.py
@@ -2,16 +2,14 @@ import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-APP_ENV = os.getenv('APP_ENV', 'development')
 DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'Panribou1')
 DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
 
-SQLALCHEMY_DATABASE_URI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}?sslmode=require"
 
 CONTENTFUL_SPACE_ID = 'ivu7p66elrjd'
 CONTENTFUL_ACCESS_TOKEN = 'DQPKTv3cLkLLYkiZdubD4vrLAQgy0q1tVpepW3mbSKM'
-# CONTENTFUL_MANAGEMENT_ACCESS_TOKEN = 'CFPAT-0txYVnkTk_rekQy1_bdHjFJDaFWQ8KtiqpKM4g0WpdI'
 
 del os
