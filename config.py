f7519be8cbcb28d8544c230f53f0e3c1bde34135
Clément <clem.roc5@gmail.com>
2023-05-22 20:26:28 +0200

    Deleting env variables and assets

--- config.py
+++ config.py
@@ -2,23 +2,16 @@ import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-# HEROKU ENV
-# DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'bvevoziovkbuck')
-# DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'ad9c35854e81b21853829f1e13fee7d483efb7e454e1c56179e53aee2ab0fa3e')
-# DATABASE_HOST = os.getenv('DATABASE_HOST', 'ec2-34-242-154-118.eu-west-1.compute.amazonaws.com')
-# DATABASE_NAME = os.getenv('DATABASE_NAME', 'dcm44cv9ftqjk6')
-#
-# SQLALCHEMY_DATABASE_URI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}?sslmode=require"
 
 # DEV ENV
-DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'Panribou1')
-DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
-DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'username')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'password')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'host')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'name')
 
 SQLALCHEMY_DATABASE_URI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 
-CONTENTFUL_SPACE_ID = 'ivu7p66elrjd'
-CONTENTFUL_ACCESS_TOKEN = 'DQPKTv3cLkLLYkiZdubD4vrLAQgy0q1tVpepW3mbSKM'
+CONTENTFUL_SPACE_ID = 'Space ID here'
+CONTENTFUL_ACCESS_TOKEN = 'Access Token here'
 
 del os
