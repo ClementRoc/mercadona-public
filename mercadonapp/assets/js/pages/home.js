f917eeccc5ff37a6ab92c4afda1a30bc56739ba7
Clément Rocolle <clem.roc5@gmail.com>
2023-05-22 20:29:46 +0200

    Delete mercadonapp/assets directory

--- mercadonapp/assets/js/pages/home.js
+++
@@ -1,6 +0,0 @@
-const $promoBtn = $('.presentation-btn')
-
-// Set the localStorage click on true when the homepage button is clicked
-$promoBtn.on('click', function() {
-    localStorage.setItem('click', 'true')
-})
